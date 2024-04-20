10dc9adb4bd8a2a9685a76a517c43b9c76f91cbf
Hugh Rawlinson <hughr2005@gmail.com>
2024-04-20 23:20:59 +0200

    Add github pages deploy

--- packages/tildagon-app-directory-site/astro.config.mjs
+++ packages/tildagon-app-directory-site/astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://emfcamp.github.io/",
+  base: "badge-2024-app-store",
 });
