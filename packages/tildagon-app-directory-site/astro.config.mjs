5eeb8e4e968cbb70f1c2d7fb61cbd1a287cb1518
Hugh Rawlinson <hughr2005@gmail.com>
2024-05-28 02:31:10 +0200

    Visual tweaks

--- packages/tildagon-app-directory-site/astro.config.mjs
+++ packages/tildagon-app-directory-site/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://apps.badge.emfcamp.org",
   //base: process.env.CI ? "/badge-2024-app-store" : undefined,
+  integrations: [
+    icon({ include: { openmoji: ["backhand-index-pointing-down"] } }),
+  ],
 });
