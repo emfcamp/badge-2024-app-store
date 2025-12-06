5609ed8d5c0cc0de2f67e3da1feecb3490855316
Hugh Rawlinson <hughr2005@gmail.com>
2025-12-06 23:52:34 +0100

    switch to bun's new router
    temporarily, it will make switching to a NodeJS router easier.

--- packages/tildagon-app-directory-api/index.ts
+++ packages/tildagon-app-directory-api/index.ts
@@ -5,61 +5,43 @@ function parseUrlSegments(url: string) {
   return urlSegments.slice(1);
 }
 
-async function handleApps(urlSegments: string[], request: Request) {
-  if (urlSegments[0]) {
-    const code = urlSegments[0];
-    const app = await CachedRegistryManager.getApp(code);
-    return new Response(
-      JSON.stringify(app.type === "success" ? app.value : app.failure),
-      {
-        status: app.type === "success" ? 200 : 404,
+const PORT = process.env.PORT || 3000;
+const server = Bun.serve({
+  port: PORT,
+  routes: {
+    "/v1/apps": async (req) => {
+      const apps = await CachedRegistryManager.listApps();
+
+      return new Response(JSON.stringify({ items: apps, count: apps.length }), {
+        status: 200,
         headers: { "Content-Type": "application/json" },
-      },
-    );
-  }
-
-  const apps = await CachedRegistryManager.listApps();
-
-  return new Response(JSON.stringify({ items: apps, count: apps.length }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
-
-async function routeAPI(urlSegments: string[], request: Request) {
-  switch (urlSegments[0]) {
-    case "apps":
-      return await handleApps(urlSegments.slice(1), request);
-    case "failures":
+      });
+    },
+    "/v1/apps/:id": async (req) => {
+      const code = req.params.id;
+      const app = await CachedRegistryManager.getApp(code);
+      return new Response(
+        JSON.stringify(app.type === "success" ? app.value : app.failure),
+        {
+          status: app.type === "success" ? 200 : 404,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    },
+    "/v1/failures": async () => {
       const failures = await CachedRegistryManager.listErrors();
       return new Response(
         JSON.stringify({ items: failures, count: failures.length }),
         { status: 200, headers: { "Content-Type": "application/json" } },
       );
-    default:
-      return new Response("Not found", { status: 404 });
-  }
-}
-
-const PORT = process.env.PORT || 3000;
-const server = Bun.serve({
-  port: PORT,
+    },
+  },
   async fetch(request) {
-    console.log(
-      `\U0001f3d5\ufe0f tildagon-app-directory-api listening on ${PORT}`,
-    );
-    const urlSegments = parseUrlSegments(request.url);
-
-    switch (urlSegments[0]) {
-      case "v1":
-        return await routeAPI(urlSegments.slice(1), request);
-      default:
-        return new Response("Not found", { status: 404 });
-    }
+    return new Response("Not found", { status: 404 });
   },
 });
 
 const pidFile = `${process.cwd()}/.server.pid`;
 Bun.write(pidFile, process.pid.toString());
 console.log(`Server process pid: ${process.pid}`);
-console.log(`Server running at ${server.port}`);
+console.log(`Server running at ${server.url}`);
