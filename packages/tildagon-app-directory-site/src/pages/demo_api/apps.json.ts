137058b784296e744d5ab0fca812670025e2d257
Hugh Rawlinson <hughr2005@gmail.com>
2026-05-27 20:54:42 +0200

    revert api to single category

--- packages/tildagon-app-directory-site/src/pages/demo_api/apps.json.ts
+++ packages/tildagon-app-directory-site/src/pages/demo_api/apps.json.ts
@@ -1,8 +1,21 @@
+import type { TildagonAppRelease } from "tildagon-app";
 import { CachedRegistryManager } from "tildagon-app-directory-api";
 
 const apps = await CachedRegistryManager.listApps();
 
-const data = { items: apps, count: apps.length };
+const data = {
+  items: apps.map((app: TildagonAppRelease) => ({
+    ...app,
+    manifest: {
+      ...app.manifest,
+      app: {
+        ...app.manifest.app,
+        category: app.manifest.app.category[0],
+      },
+    },
+  })),
+  count: apps.length,
+};
 
 export async function GET() {
   return new Response(JSON.stringify(data));
