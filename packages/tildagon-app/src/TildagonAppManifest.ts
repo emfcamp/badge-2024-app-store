91213643deacf0898a5ad1dd02b487859b909ef3
Hugh Rawlinson <hughr2005@gmail.com>
2026-06-07 00:19:15 +0200

    feat: hardware support

--- packages/tildagon-app/src/TildagonAppManifest.ts
+++ packages/tildagon-app/src/TildagonAppManifest.ts
@@ -12,6 +12,57 @@ export const TildagonAppCategory = z.enum([
 
 export type TildagonAppCategory = z.infer<typeof TildagonAppCategory>;
 
+export const HexpansionIdentifier = z.object({
+  name: z.string().optional(),
+  creator: z.string().optional(),
+  url: z.url().optional(),
+  vid: z.string(),
+  pid: z.string(),
+});
+
+export const Capability = z.string();
+
+export const HexpansionDefinition = z.object({
+  identifier: HexpansionIdentifier,
+  capabilities: z.array(Capability),
+});
+
+export const Frontboard2024 = z.object({
+  name: z.literal("2024 Frontboard"),
+  capabilities: z.tuple([]),
+});
+
+export const Frontboard2026 = z.object({
+  name: z.literal("2026 Frontboard"),
+  capabilities: z.tuple([]),
+});
+
+export const TildagonOSMinimumVersion = z.object({
+  type: "TildagonOSMinimumVersion",
+  version: z.string(),
+});
+
+export const ProvidedCapability = z.object({
+  type: "ProvidedCapability",
+  capability: Capability,
+});
+
+export const Frontboard = z.discriminatedUnion("name", [
+  Frontboard2024,
+  Frontboard2026,
+]);
+
+export const FrontboardIdentifier = z.object({
+  name: z.union(Frontboard.options.map((option) => option.shape.name)),
+});
+
+export const TildagonAppCapabilityAssociations = z.array(
+  z.object({
+    required: z.boolean(),
+    feature: z.union([FrontboardIdentifier, HexpansionIdentifier, Capability]),
+  }),
+);
+
 export const TildagonAppManifestSchema = z.object({
   app: z.object({
     name: z.string(),
@@ -26,6 +77,7 @@ export const TildagonAppManifestSchema = z.object({
     url: z.string(),
     description: z.string(),
     version: z.string(),
+    capabilities: TildagonAppCapabilityAssociations.optional(),
   }),
 });
 
