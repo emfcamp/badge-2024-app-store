597022bd3bc847bd8423ae66220b3f963cd8d68d
Hugh Rawlinson <hughr2005@gmail.com>
2026-06-10 20:14:11 +0200

    feat: dockerize

--- packages/tildagon-app-directory-site/astro.config.mjs
+++ packages/tildagon-app-directory-site/astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  adapter: node({
+    mode: "standalone",
+  }),
+  output: "server",
   site: "https://apps.badge.emfcamp.org",
   //base: process.env.CI ? "/badge-2024-app-store" : undefined,
   integrations: [
