4c0c179da022012d35e0d7ad5f35565328be21df
Hugh Rawlinson <hughr2005@gmail.com>
2026-07-03 20:30:26 +0200

    fix: switch the astro to middleware mode

--- packages/tildagon-app-directory-site/astro.config.mjs
+++ packages/tildagon-app-directory-site/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     ? { output: "static" }
     : {
         adapter: node({
-          mode: "standalone",
+          mode: "middleware",
         }),
         output: "server",
       }),
