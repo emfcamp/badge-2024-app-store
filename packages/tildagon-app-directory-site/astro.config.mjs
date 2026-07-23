b2f27ad4af1f9924e248d74af5a19018b7879df7
Hugh Rawlinson <hughr2005@gmail.com>
2026-07-23 12:03:30 +0200

    chore: clean up github pages

--- packages/tildagon-app-directory-site/astro.config.mjs
+++ packages/tildagon-app-directory-site/astro.config.mjs
@@ -2,19 +2,12 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import node from "@astrojs/node";
 
-// Use static mode for GitHub Pages, server mode for Docker SSR
-const isStatic = process.env.APP_STORE_STATIC === "true";
-
 // https://astro.build/config
 export default defineConfig({
-  ...(isStatic
-    ? { output: "static" }
-    : {
-        adapter: node({
-          mode: "middleware",
-        }),
-        output: "server",
-      }),
+  adapter: node({
+    mode: "middleware",
+  }),
+  output: "server",
   site: "https://apps.badge.emfcamp.org",
   //base: process.env.CI ? "/badge-2024-app-store" : undefined,
   integrations: [
